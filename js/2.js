1e0e796addd03f069bad1261d1b467b748ea50e3
Chernat <chernous1994@gmail.com>
2019-01-28 11:27:57 +0200

    Update 2.js

--- js/2.js
+++ js/2.js
@@ -6,15 +6,19 @@ function creatTable() {
   var table = document.createElement('table');
   var tablePlace = document.getElementById('second-task');
   tablePlace.appendChild(table);
+
   for (var i = 0; i < 10; i++) {
     var tr = document.createElement('tr');
     table.appendChild(tr);
+
     for (var j = 0; j < 10; j++) {
       var td = document.createElement('td');
       td.innerText = (j + 1) + ':' + (j + 1);
+      
       if (j === i) {
         td.style.backgroundColor = 'red';
       }
+
       tr.appendChild(td);
     }
   }
